3ddc7d36b6babf1eb6459c012d4b472fe9782e99
fdedinec <Florent.de-Dinechin@insa-lyon.fr>
2022-02-03 18:07:00 +0100

    bug in PAG compilation, commenting it for now

--- doc/web/install_scripts/install-full-gitmaster-on-focal64.sh
+++ doc/web/install_scripts/install-full-gitmaster-on-focal64.sh
@@ -1,35 +1,39 @@
 #!/bin/bash
 
+# install the dependencies that are available in a Debian distribution
 yes | sudo apt update && sudo DEBIAN_FRONTEND=noninteractive apt install -y subversion git cmake sollya wget g++ libsollya-dev flex bison libboost-all-dev autotools-dev autoconf automake f2c libblas-dev liblapack-dev libtool liblpsolve55-dev lp-solve
 
-FLOPOCO_INSTALL_DIR=$PWD
+# For the other dependencies, this script compiles everything it needs in the current directory (no system-wide install)
+
+BASE_DIR=$PWD
 
 
-cd $FLOPOCO_INSTALL_DIR
+cd $BASE_DIR
 #WCPG
 git clone https://github.com/fixif/WCPG.git && cd WCPG && WCPG_PREFIX_DIR=$PWD/install && sh autogen.sh && ./configure --prefix=$WCPG_PREFIX_DIR && make -j4 && make install 
 
-cd $FLOPOCO_INSTALL_DIR
+cd $BASE_DIR
 # ScaLP with the LPSolve backend, the only backend that installs automatically (but it is not very good).
 #  for other backends see see the ScaLP documentation -- one example is below
 svn checkout https://digidev.digi.e-technik.uni-kassel.de/home/svn/scalp/ && cd scalp/trunk && SCALP_PREFIX_DIR=$PWD && mkdir build && cd build && cmake -DUSE_LPSOLVE=ON -DLPSOLVE_LIBRARIES="/usr/lib/lp_solve/liblpsolve55.so" -DLPSOLVE_INCLUDE_DIRS="/usr/include/" .. && make -j4
 
 # ScaLP with the SCIP backend: 
 # SCIP needs to be downloaded separately, but then uncommenting the following three lines should get it working
-#SCIP_DIR=$FLOPOCO_INSTALL_DIR/scipoptsuite-6.0.0 # modify just this
+#SCIP_DIR=$BASE_DIR/scipoptsuite-6.0.0 # modify just this
 # cd $SCIP_DIR &&  mkdir build && cd build && cmake -DCMAKE_INSTALL_PREFIX=$SCIP_DIR/install .. && make -j4 && make install
-# cd $FLOPOCO_INSTALL_DIR
+# cd $BASE_DIR
 # svn checkout https://digidev.digi.e-technik.uni-kassel.de/home/svn/scalp/ && cd scalp/trunk && SCALP_PREFIX_DIR=$PWD  && mkdir build && cd build && cmake -DUSE_SCIP=ON -DSCIP_LIBRARIES="$SCIP_DIR/install/lib/" -DSCIP_INCLUDE_DIRS="$SCIP_DIR/install/include/" .. && make -j4
 
-cd $FLOPOCO_INSTALL_DIR
+cd $BASE_DIR
 # PAGSuite for advanced shift-and-add SCM and MCM operators
-git clone https://gitlab.com/kumm/pagsuite.git && cd pagsuite && PAG_PREFIX_DIR=$PWD && mkdir build && cd build && cmake .. -DSCALP_PREFIX_PATH=$SCALP_PREFIX_DIR && make -j4
+# Commented for now because it is broken
+# git clone https://gitlab.com/kumm/pagsuite.git && cd pagsuite && PAG_PREFIX_DIR=$PWD && mkdir build && cd build && cmake .. -DSCALP_PREFIX_PATH=$SCALP_PREFIX_DIR && make -j4
 
-cd $FLOPOCO_INSTALL_DIR
+cd $BASE_DIR
 #Finally FloPoCo itself, 
 git clone git@gitlab.com:flopoco/flopoco.git
 
-cd flopoco && mkdir build && cd build && cmake  -DWCPG_PREFIX_DIR="$WCPG_PREFIX_DIR"  -DSCALP_PREFIX_DIR="$SCALP_PREFIX_DIR" -DPAG_PREFIX_DIR="$FLOPOCO_INSTALL_DIR/pagsuite" .. && make -j4 &&  cd $FLOPOCO_INSTALL_DIR
+cd flopoco && mkdir build && cd build && cmake  -DWCPG_PREFIX_DIR="$WCPG_PREFIX_DIR"  -DSCALP_PREFIX_DIR="$SCALP_PREFIX_DIR" -DPAG_PREFIX_DIR="$BASE_DIR/pagsuite" .. && make -j4 &&  cd $BASE_DIR
 
 # build the html documentation in doc/web. 
 cd flopoco/build
